28c0b3e676378e2158315e529a9f6eedc58af6ef
Douwe Gnodde <douwe@skrepr.com>
2020-09-02 15:49:49 +0200

    Detail is added to soap exception if the throable is a SoapFault

--- src/Phpro/SoapClient/Exception/SoapException.php
+++ src/Phpro/SoapClient/Exception/SoapException.php
@@ -2,6 +2,8 @@
 
 namespace Phpro\SoapClient\Exception;
 
+use Throwable;
+
 /**
  * Class SoapException
  *
@@ -10,12 +12,34 @@ namespace Phpro\SoapClient\Exception;
 class SoapException extends RuntimeException
 {
     /**
+     * @var mixed
+     */
+    private $detail;
+
+    public function __construct($message = "", $code = 0, Throwable $previous = null, $detail = null)
+    {
+        $this->detail = $detail;
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
      * @param \Throwable $throwable
      *
      * @return SoapException
      */
     public static function fromThrowable(\Throwable $throwable): self
     {
-        return new self($throwable->getMessage(), (int)$throwable->getCode(), $throwable);
+        $detail = null;
+
+        if ($throwable instanceof \SoapFault) {
+            $detail = $throwable->detail;
+        }
+
+        return new self($throwable->getMessage(), (int)$throwable->getCode(), $throwable, $detail);
+    }
+
+    public function getDetail()
+    {
+        return $this->detail;
     }
 }
